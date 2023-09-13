1bee7f1de27305d5d20dc720b9db1edc99ce80c3
cjagbayani26 <30024651@student.toiohomai.ac.nz>
2023-09-13 23:43:16 +1200

    commit

--- src/SCP.js
+++ src/SCP.js
@@ -1,15 +1,16 @@
-/*import './scp.css';*/
+import React from 'react';
+import './scp.css';
 
 
 function SCP({scp})
 {
     return(
-        <div>
+        <div className="component container">
             <h2>Subject:</h2>
             <h3>ITEM #: {scp.item}</h3>
             <h2>Object Class:</h2>
             <h3>{scp.object}</h3>
-            <img src={`/${scp.icon}`} alt={scp.icon} />
+            <img src={process.env.PUBLIC_URL + scp.icon} alt={scp.icon} />
             <h2>Containment Procedure: </h2>
             <p>{scp.containment}</p>
             <h2>Description:</h2>
